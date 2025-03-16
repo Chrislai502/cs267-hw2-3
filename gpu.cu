c0b7207c4578e6276d7ceb0d7399fb022237c72b
Yarden Goraly <ygoraly@login17.chn.perlmutter.nersc.gov>
2025-03-16 13:47:05 -0700

    runs on small particles, but doesn't pass correctness

--- gpu.cu
+++ gpu.cu
@@ -1,10 +1,15 @@
 #include "common.h"
 #include <cuda.h>
+#include <thrust/scan.h>
+#include <thrust/execution_policy.h>
 
 #define NUM_THREADS 256
 
 // Put any static global variables here that you will use throughout the simulation.
 int blks;
+int* bin_counts = nullptr;
+int* bin_ids = nullptr;
+particle_t* particle_ids = nullptr;
 
 __device__ void apply_force_gpu(particle_t& particle, particle_t& neighbor) {
     double dx = neighbor.x - particle.x;
@@ -22,6 +27,47 @@ __device__ void apply_force_gpu(particle_t& particle, particle_t& neighbor) {
     double coef = (1 - cutoff / r) / r2 / mass;
     particle.ax += coef * dx;
     particle.ay += coef * dy;
+}
+
+// Apply the force from neighbor to particle
+inline void apply_force(particle_t& particle, particle_t& neighbor) {
+    // Calculate Distance
+    double dx = neighbor.x - particle.x;
+    double dy = neighbor.y - particle.y;
+    double r2 = dx * dx + dy * dy;
+
+    // Check if the two particles should interact
+    if (r2 > cutoff * cutoff)
+        return;
+
+    r2 = fmax(r2, min_r * min_r);
+    double r = sqrt(r2);
+
+    // Very simple short-range repulsive force
+    double coef = (1 - cutoff / r) / r2 / mass;
+    particle.ax += coef * dx;
+    particle.ay += coef * dy;
+}
+
+// Integrate the ODE
+inline void move(particle_t& p, double size) {
+    // Slightly simplified Velocity Verlet integration
+    // Conserves energy better than explicit Euler method
+    p.vx += p.ax * dt;
+    p.vy += p.ay * dt;
+    p.x += p.vx * dt;
+    p.y += p.vy * dt;
+
+    // Bounce from walls
+    while (p.x < 0 || p.x > size) {
+        p.x = p.x < 0 ? -p.x : 2 * size - p.x;
+        p.vx = -p.vx;
+    }
+
+    while (p.y < 0 || p.y > size) {
+        p.y = p.y < 0 ? -p.y : 2 * size - p.y;
+        p.vy = -p.vy;
+    }
 }
 
 __global__ void compute_forces_gpu(particle_t* particles, int num_parts) {
@@ -65,22 +111,165 @@ __global__ void move_gpu(particle_t* particles, int num_parts, double size) {
     }
 }
 
+void update_bin_ids_and_particle_ids(particle_t* parts, int num_parts, double size) {
+
+    double cellsize = cutoff;
+    int gridsize = (size / cellsize) + 1;
+    int num_bins = gridsize * gridsize;
+
+    if (bin_ids && particle_ids) {
+        std::cout << "here 1.1" << std::endl;
+        delete[] bin_ids;
+        std::cout << "here 1.2" << std::endl;
+        delete[] particle_ids;
+        std::cout << "here 1.3" << std::endl;
+        bin_ids = new int[num_bins + 1];
+        std::cout << "here 1.4" << std::endl;
+        memset(bin_ids, 0, (num_bins + 1) * sizeof(int));
+        std::cout << "here 1.5" << std::endl;
+        particle_ids = nullptr;
+    }
+
+    // bin_ids is now an array of the index of each bin
+    thrust::inclusive_scan(thrust::host, bin_counts, bin_counts + num_bins, bin_ids);
+    particle_ids = new particle_t[num_parts + 1];
+
+    // populate particles_ids sorted by bins
+    int keep_track_array[num_bins] = {0}; // keeps track of how far into the bin the next particle should be
+
+    for (int i = 0; i < num_parts; ++i) {
+        int grid_r = parts[i].y / cellsize;
+        int grid_c = parts[i].x / cellsize;
+        int particle_idx = bin_ids[grid_r * gridsize + grid_c] + keep_track_array[grid_r * gridsize + grid_c];
+        keep_track_array[grid_r * gridsize + grid_c]++;
+        particle_ids[particle_idx] = parts[i];
+    }
+}
+
 void init_simulation(particle_t* parts, int num_parts, double size) {
     // You can use this space to initialize data objects that you may need
     // This function will be called once before the algorithm begins
     // parts live in GPU memory
     // Do not do any particle simulation here
 
+    std::cout << "here 0" << std::endl;
+    // convert parts to cpu for testing
+    particle_t* cpu_parts = new particle_t[num_parts];
+    cudaMemcpy(cpu_parts, parts, num_parts * sizeof(particle_t), cudaMemcpyDeviceToHost);
+
+
     blks = (num_parts + NUM_THREADS - 1) / NUM_THREADS;
+
+    double cellsize = cutoff;
+    int gridsize = (size / cellsize) + 1;
+
+    std::cout << "here 0.1" << std::endl;
+    int num_bins = gridsize * gridsize;
+    bin_counts = new int[num_bins];
+    memset(bin_counts, 0, num_bins * sizeof(int));
+
+    std::cout << "here 0.2" << std::endl;
+    // count the number of particles in each bin
+    for (int i = 0; i < num_parts; ++i) {
+        int grid_r = cpu_parts[i].y / cellsize;
+        int grid_c = cpu_parts[i].x / cellsize;
+        bin_counts[grid_r * gridsize + grid_c]++;
+        // std::cout << "num_bins " << num_bins << " grid_r * gridsize + grid_c " << grid_r * gridsize + grid_c << " bin_counts[grid_r * gridsize + grid_c] " << bin_counts[grid_r * gridsize + grid_c] << std::endl;
+    }
+
+    std::cout << "here 0.3" << std::endl;
+    bin_ids = new int[num_bins + 1];
+    memset(bin_ids, 0, (num_bins + 1) * sizeof(int));
+    update_bin_ids_and_particle_ids(cpu_parts, num_parts, size);
+    std::cout << "here 0.4" << std::endl;
+    delete[] cpu_parts;
 }
+
+// list of adjacencies
+static int ALL_ADJ[9][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1},
+                            {0, 1},   {1, -1}, {1, 0},  {1, 1}, {0, 0}};
 
 void simulate_one_step(particle_t* parts, int num_parts, double size) {
     // parts live in GPU memory
     // Rewrite this function
 
     // Compute forces
-    compute_forces_gpu<<<blks, NUM_THREADS>>>(parts, num_parts);
+    // compute_forces_gpu<<<blks, NUM_THREADS>>>(parts, num_parts);
 
     // Move particles
-    move_gpu<<<blks, NUM_THREADS>>>(parts, num_parts, size);
+    // move_gpu<<<blks, NUM_THREADS>>>(parts, num_parts, size);
+
+    std::cout << "here 1" << std::endl;
+    // convert parts to cpu for testing
+    particle_t* cpu_parts = new particle_t[num_parts];
+    cudaMemcpy(cpu_parts, parts, num_parts * sizeof(particle_t), cudaMemcpyDeviceToHost);
+
+    std::cout << "here 2" << std::endl;
+
+    double cellsize = cutoff;
+    int gridsize = (size / cellsize) + 1;
+    int num_bins = gridsize * gridsize;
+
+
+    for (int b = 0; b < num_bins; ++b) { //TODO: figure out if the -1 is necessary as last element of bin_ids doesn't point to anything
+        // iterate through particles in bin
+        for (int par = bin_ids[b]; par < bin_ids[b] + bin_counts[b]; ++par) {
+            particle_t* cur_particle = &particle_ids[par];
+            cur_particle->ax = cur_particle->ay = 0;
+            
+            std::cout << "here 3" << std::endl;
+            int r = cur_particle->y / cellsize;
+            int c = cur_particle->x / cellsize;
+            std::cout << "here 4" << std::endl;
+
+            for (int i = 0; i < 9; ++i) {
+                int new_r = r + ALL_ADJ[i][0];
+                int new_c = c + ALL_ADJ[i][1];
+                
+                int neigh_bin_idx = new_r * gridsize + new_c;
+                if (neigh_bin_idx >= 0 && neigh_bin_idx < num_bins) {
+                    int particle_idx = bin_ids[neigh_bin_idx];
+                    // neighbors are particles in neighboring cells
+                    std::cout << "here 5" << std::endl;
+                    for (int neigh = particle_idx; neigh < particle_idx + bin_counts[neigh_bin_idx]; ++neigh) {
+                        particle_t* neigh_particle = &particle_ids[neigh];
+                        apply_force(*cur_particle, *neigh_particle);
+                        std::cout << "here 6" << std::endl;
+                    }
+                }
+            }            
+        }
+    }
+
+    std::cout << "here 7" << std::endl;
+    // move and adjust grid assignments for each particle
+    for (int i = 0; i < num_parts; ++i) {
+        particle_t* particle = &particle_ids[i];
+        int r = particle->y / cellsize;
+        int c = particle->x / cellsize;
+        
+        // move the particle to its new grid cell
+        move(*particle, size);
+
+        std::cout << "here 8" << std::endl;
+        // compute intended particle grid position
+        int n_r = particle->y / cellsize;
+        int n_c = particle->x / cellsize;
+        std::cout << "here 8.1" << std::endl;
+        // check if particle is still in grid cell
+        if (r != n_r || c != n_c) {
+            
+            // decrement old bin count
+            bin_counts[r * gridsize + c]--;
+            std::cout << "here 8.2" << std::endl;
+            // incrememt new bin count
+            std::cout << "num_bins " << num_bins << " n_r * gridsize + n_c " << n_r * gridsize + n_c << std::endl;
+            bin_counts[n_r * gridsize + n_c]++;
+        }
+        std::cout << "here 9" << std::endl;
+    }
+    // reset particle_ids and bin_ids
+    update_bin_ids_and_particle_ids(cpu_parts, num_parts, size);
+    std::cout << "here 10" << std::endl;
+    delete[] cpu_parts;
 }
