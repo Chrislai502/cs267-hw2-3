f80c7641345c1ad8ba7af595d0eee60769e3e8d9
Alec Li <smartspot2@gmail.com>
2025-03-21 14:15:06 -0700

    Add starter code data

--- out/parse-data.py
+++ out/parse-data.py
@@ -16,7 +16,52 @@ DATA_IN = "./update_particles.3.txt"
 DATA_OUT = "./update_particles.3.csv"
 TIMER_DATA_IN = "./update_particles.3.timers.txt"
 TIMER_DATA_OUT = "./update_particles.3.timers.csv"
+STARTER_DATA_IN = "./starter.txt"
+STARTER_DATA_OUT = "./starter.csv"
 
+def parse_starter_data():
+    with open(STARTER_DATA_IN, "r", encoding="utf-8") as f:
+        data = f.readlines()
+
+    data_by_particles = {}
+
+    for line in data:
+        match = re.search(
+            r"Simulation Time = ([\.\d]+) seconds for (\d+) particles", line
+        )
+        if match is None:
+            print("Unknown line", line)
+            continue
+
+        time_secs = float(match.group(1))
+        num_particles = int(match.group(2))
+
+        if num_particles not in data_by_particles:
+            data_by_particles[num_particles] = []
+
+        data_by_particles[num_particles].append(time_secs)
+
+    # compute averages
+    all_rows = []
+    csv_rows = []
+    for num_particles in sorted(data_by_particles):
+        times = data_by_particles[num_particles]
+        mean_time_secs = sum(times) / len(times)
+        csv_rows.append((num_particles, mean_time_secs))
+        all_rows.extend([[num_particles, time_secs] for time_secs in times])
+
+    with open(STARTER_DATA_OUT, "w", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(["num_particles", "time_secs"])
+        writer.writerows(csv_rows)
+
+    arr = np.array(all_rows)
+    plt.xlabel("# particles")
+    plt.ylabel("Time (s)")
+    plt.xscale("log")
+    plt.yscale("log")
+    sns.lineplot(x=arr[:, 0], y=arr[:, 1])
+    plt.show()
 
 def parse_data():
     with open(DATA_IN, "r", encoding="utf-8") as f:
@@ -151,4 +196,5 @@ def parse_timer_data():
 
 if __name__ == "__main__":
     parse_data()
+    parse_starter_data()
     parse_timer_data()
